25b110a9b6b2845c42e6ec4357652bd88edfdc4c
Diane <vasya26112000@gmail.com>
2024-03-12 02:19:36 +0500

    3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿Console.WriteLine("Введите элементы массива через запятую:");
+string[] inputArray = Console.ReadLine().Split(',');
+
+string[] FilterStrings(string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+
+    int index = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+
+    return resultArray;
+}
+
+string[] resultArray = FilterStrings(inputArray);
+
+Console.WriteLine("Результат:");
+Console.WriteLine(string.Join(", ", resultArray));
